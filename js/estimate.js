763165a76b62b2fa365bd2e060609faffbb76010
philippe streiff <45629903+chipalou@users.noreply.github.com>
2019-06-21 17:25:56 +0200

    Update estimate.js

--- js/estimate.js
+++ js/estimate.js
@@ -38,7 +38,7 @@ document.getElementById('save').addEventListener('click', function(){
     if (tcSelector.value=="6") { var tctemp= 13 } 
     if (tcSelector.value=="7") { var tctemp= 21 } 
     
-    var wsjfval = ubvtemp
+    var wsjfval = tctemp
     /*var wsjfval = ((ubvSelector.value+tcSelector.value+rroeSelector.value)/jsSelector.value)*/
     /* return t.set('card', 'shared',  'wsjf', (parseFloat(Math.round(wsjfval) * 100) / 100).toFixed(2)) */
    /* var wsjfval = ((ubvSelector.value+tcSelector.value+rroeSelector.value)/jsSelector.value) */
