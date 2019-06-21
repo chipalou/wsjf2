d041e119f522a6267672c0e918abe73d0ca7eb68
philippe streiff <45629903+chipalou@users.noreply.github.com>
2019-06-21 17:10:29 +0200

    Update estimate.js

--- js/estimate.js
+++ js/estimate.js
@@ -63,8 +63,51 @@ document.getElementById('save').addEventListener('click', function(){
     } 
       if (tcSelector.value=="7") {
       var tctemp= 21
-} 
-    var wsjfval = ubvtemp+tctemp
+    } 
+    if (rroeSelector.value=="1") {
+      var tctemp= 1
+    } 
+    if (rroeSelector.value=="2") {
+      var tctemp = 2
+    } 
+     if (rroeSelector.value=="3") {
+      var tctemp= 3
+    } 
+    if (rroeSelector.value=="4") {
+      var tctemp= 5
+    } 
+      if (rroeSelector.value=="5") {
+      var tctemp= 8
+    } 
+      if (rroeSelector.value=="6") {
+      var tctemp= 13
+    } 
+      if (rroeSelector.value=="7") {
+      var tctemp= 21
+    } 
+    if (jsSelector.value=="1") {
+      var tctemp= 1
+    } 
+    if (jsSelector.value=="2") {
+      var tctemp = 2
+    } 
+    if (jsSelector.value=="3") {
+      var tctemp= 3
+    } 
+    if (jsSelector.value=="4") {
+      var tctemp= 5
+    } 
+    if (jsSelector.value=="5") {
+      var tctemp= 8
+    } 
+    if (jsSelector.value=="6") {
+      var tctemp= 13
+    } 
+    if (jsSelector.value=="7") {
+      var tctemp= 21
+    } 
+    
+    var wsjfval = (ubvtemp+tctemp+rroetemp)
     /*var wsjfval = ((ubvSelector.value+tcSelector.value+rroeSelector.value)/jsSelector.value)*/
     /* return t.set('card', 'shared',  'wsjf', (parseFloat(Math.round(wsjfval) * 100) / 100).toFixed(2)) */
    /* var wsjfval = ((ubvSelector.value+tcSelector.value+rroeSelector.value)/jsSelector.value) */
