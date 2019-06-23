861c018016be578f2afe09e49c6e27500a1ad2f8
philippe streiff <45629903+chipalou@users.noreply.github.com>
2019-06-23 14:47:22 +0200

    Update estimate.js

--- js/estimate.js
+++ js/estimate.js
@@ -23,7 +23,7 @@ document.getElementById('save').addEventListener('click', function(){
   })
   .then(function(){
     if (ubvSelector.value=="") { var ubvtemp= 0 }  
-    if (ubvSelector.value==" ") { var ubvtemp= 0 } 
+    if (ubvSelector.value=="1") { var ubvtemp= 0 } 
     if (ubvSelector.value=="2") { var ubvtemp= 1 } 
     if (ubvSelector.value=="3") { var ubvtemp= 2 } 
     if (ubvSelector.value=="4") { var ubvtemp= 3 } 
@@ -33,34 +33,34 @@ document.getElementById('save').addEventListener('click', function(){
     if (ubvSelector.value=="8") { var ubvtemp= 21 } 
   
     if (tcSelector.value=="") { var tctemp= 0 }  
-    if (tcSelector.value==" ") { var tctemp= 0 } 
-    if (tcSelector.value=="1") { var tctemp= 1 } 
-    if (tcSelector.value=="2") { var tctemp= 2 } 
-    if (tcSelector.value=="3") { var tctemp= 3 } 
-    if (tcSelector.value=="4") { var tctemp= 5 } 
-    if (tcSelector.value=="5") { var tctemp= 8 } 
-    if (tcSelector.value=="6") { var tctemp= 13 } 
-    if (tcSelector.value=="7") { var tctemp= 21 } 
+    if (tcSelector.value=="1") { var tctemp= 0 } 
+    if (tcSelector.value=="2") { var tctemp= 1 } 
+    if (tcSelector.value=="3") { var tctemp= 2 } 
+    if (tcSelector.value=="4") { var tctemp= 3 } 
+    if (tcSelector.value=="5") { var tctemp= 5 } 
+    if (tcSelector.value=="6") { var tctemp= 8 } 
+    if (tcSelector.value=="7") { var tctemp= 13 } 
+    if (tcSelector.value=="8") { var tctemp= 21 } 
     
    if (rroeSelector.value=="") { var rroetemp= 0 }  
-    if (rroeSelector.value==" ") { var rroetemp= 0 }
-    if (rroeSelector.value=="1") { var rroetemp= 1 } 
-    if (rroeSelector.value=="2") { var rroetemp= 2 } 
-    if (rroeSelector.value=="3") { var rroetemp= 3 } 
-    if (rroeSelector.value=="4") { var rroetemp= 5 } 
-    if (rroeSelector.value=="5") { var rroetemp= 8 } 
-    if (rroeSelector.value=="6") { var rroetemp= 13 } 
-    if (rroeSelector.value=="7") { var rroetemp= 21 } 
+    if (rroeSelector.value=="1") { var rroetemp= 0 }
+    if (rroeSelector.value=="2") { var rroetemp= 1 } 
+    if (rroeSelector.value=="3") { var rroetemp= 2 } 
+    if (rroeSelector.value=="4") { var rroetemp= 3 } 
+    if (rroeSelector.value=="5") { var rroetemp= 5 } 
+    if (rroeSelector.value=="6") { var rroetemp= 8 } 
+    if (rroeSelector.value=="7") { var rroetemp= 13 } 
+    if (rroeSelector.value=="8") { var rroetemp= 21 } 
     
     if (jsSelector.value=="") { var jstemp= 0 }  
-    if (jsSelector.value==" ") { var jstemp= 0 } 
-    if (jsSelector.value=="1") { var jstemp= 1 } 
-    if (jsSelector.value=="2") { var jstemp= 2 } 
-    if (jsSelector.value=="3") { var jstemp= 3 } 
-    if (jsSelector.value=="4") { var jstemp= 5 } 
-    if (jsSelector.value=="5") { var jstemp= 8 } 
-    if (jsSelector.value=="6") { var jstemp= 13 } 
-    if (jsSelector.value=="7") { var jstemp= 21 } 
+    if (jsSelector.value=="1") { var jstemp= 0 } 
+    if (jsSelector.value=="2") { var jstemp= 1 } 
+    if (jsSelector.value=="3") { var jstemp= 2 } 
+    if (jsSelector.value=="4") { var jstemp= 3 } 
+    if (jsSelector.value=="5") { var jstemp= 5 } 
+    if (jsSelector.value=="6") { var jstemp= 8 } 
+    if (jsSelector.value=="7") { var jstemp= 13 } 
+    if (jsSelector.value=="8") { var jstemp= 21 } 
     
     var wsjfval = ((ubvtemp + tctemp + rroetemp)/ jstemp ) 
     /*var wsjfval = ((ubvSelector.value+tcSelector.value+rroeSelector.value)/jsSelector.value)*/
